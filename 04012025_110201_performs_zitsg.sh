08c4c4fcb873677026752a489f12f8b9fe11598e
mxedrioni <goguataliko@gmail.com>
2025-01-04 11:02:01 +0000

    Changing performs

---
+++ 04012025_110201_performs_zitsg.sh
@@ -0,0 +1,1 @@
+A 27 year old heir to a sausage empire was handed a ticket for 116,000 pounds for driving at 80 km/hr in a 40 km/hr zone. This is because the speeding tickets in Finland are based on how much money a person makes
